0765e011c9a3e994b94f9c770af6f50a7b936793
Vincent-thilash <116793735+Vincent-thilash@users.noreply.github.com>
2024-07-23 16:53:08 +0530

    removed packages

--- src/server/index.js
+++ src/server/index.js
@@ -17,23 +17,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json("hello");
 });
-
 app.post("/login", signin);
-
-app.post("/register", async (req, res) => {
-  console.log("Received /register request", req.body);
-  try {
-    const result = await signup(req.body);
-    console.log("Registration result:", result);
-    res.status(200).json(result);
-  } catch (error) {
-    console.error("Error during registration:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
+app.post("/register", signup);
 app.put("/forget-password", forgetPassword);
-
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
